c20bb4f714452297cd1fa1f0384bcac9553e7621
Minji Ji <145173900+JIMINJI1@users.noreply.github.com>
2024-09-26 09:53:56 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goal):
+    idx1,idx2=0,0
+    
+    for i in goal:
+        if idx1 < len(cards1) and i == cards1[idx1]:
+            idx1 = idx1+1
+        
+        elif idx2 < len(cards2) and i == cards2[idx2]:
+            idx2 = idx2+1
+        else :
+            return "No"
+        
+    return "Yes"
